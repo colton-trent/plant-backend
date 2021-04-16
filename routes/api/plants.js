46732df16c735011e94a92c396e407a2da70fccf
Colton Trent <coldac56@gmail.com>
2021-04-16 14:09:59 -0400

    refactor

--- routes/api/plants.js
+++ routes/api/plants.js
@@ -5,6 +5,5 @@ const plantsCtrl = require('../../controllers/plants');
 router.get('/', plantsCtrl.index);
 router.post('/', plantsCtrl.create);
 router.delete('/:id', plantsCtrl.delete);
-router.get('/:id', plantsCtrl.show);
 
 module.exports = router;
